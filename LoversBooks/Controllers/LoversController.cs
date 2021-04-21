9421aa591877db866d796e12fe34a353067f246e
Leif Linde <tristyn.linde@gmail.com>
2021-04-21 18:49:23 -0500

    update README

--- LoversBooks/Controllers/LoversController.cs
+++ LoversBooks/Controllers/LoversController.cs
@@ -8,7 +8,7 @@ using Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace LoversPizza
+namespace LoversBooks.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
